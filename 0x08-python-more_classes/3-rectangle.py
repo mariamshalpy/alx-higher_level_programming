5226244fc4fad70272b9f975bc59048aa437c1c3
Mariam Shalaby <98620529+mariamshalpy@users.noreply.github.com>
2023-10-01 16:34:33 +0300

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,67 +1,124 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""Defines rectangel class"""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """
+    Class that defines properties
 
+    Attributes:
+
+        width (int): width
+        height (int): height
+    """
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
+        """Creates new instances
 
         Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
+            width (int, optional): width
+            height (int, optional): height
         """
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
-        """Get/set the width of the Rectangle."""
-        return self.__width
+        """Width getter
 
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        Returns:
+
+            int: the width
+        """
+        return self.__width
 
     @property
     def height(self):
-        """Get/set the height of the Rectangle."""
+        """height getter.
+
+        Returns:
+            int: the height
+        """
+
         return self.__height
+
+    @width.setter
+    def width(self, value):
+        """setter
+
+        Args:
+            value (int): width
+
+        Raises:
+            TypeError: if width  not an integer.
+            ValueError: if width < 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
     @height.setter
     def height(self, value):
+        """setter
+
+        Args:
+            value (int): height
+
+        Raises:
+            TypeError: if height  not an integer.
+            ValueError: if height < than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
+
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     def area(self):
-        """Return the area of the Rectangle."""
-        return (self.__width * self.__height)
+        """Calculates the area
+
+        Returns:
+            int: area.
+        """
+        return self.__height * self.__width
 
     def perimeter(self):
-        """Return the perimeter of the Rectangle."""
-        if self.__width == 0 or self.__height == 0:
-            return (0)
-        return ((self.__width * 2) + (self.__height * 2))
+        """Calculates perimeter
+
+        Returns:
+            int: perimeter.
+        """
+
+        if self.__height == 0 or self.width == 0:
+            return 0
+        else:
+
+            return 2 * (self.__height + self.__width)
 
     def __str__(self):
-        """Return the printable representation of the Rectangle.
+        """Prints the character # .
 
-        Represents the rectangle with the # character.
+        Returns:
+            str: rectangle
         """
-        if self.__width == 0 or self.__height == 0:
-            return ("")
 
-        rect = []
+        rectangle = []
+
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
+            for j in range(self.__width):
+
+                rectangle.append("#")
+            rectangle.append("\n")
+
+        # remove blank line
+        rectangle.pop()
+
+        return "".join(rectangle)
