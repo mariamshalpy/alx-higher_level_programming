5b96da0899e455f274e7b38053cd0774251e0e9b
Mariam Shalaby <98620529+mariamshalpy@users.noreply.github.com>
2023-12-06 19:57:54 +0200

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a GET request to the URL, displays the body of the response
-curl -sX GET "$1" -L 200
+# takes in a URL, sends a request to that URL, displays the size of the body of the response
+curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
