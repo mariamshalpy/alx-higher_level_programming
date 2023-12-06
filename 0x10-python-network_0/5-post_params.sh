4aa6d95281563615c7d9c85017733bf5f4e3de69
Mariam Shalaby <98620529+mariamshalpy@users.noreply.github.com>
2023-12-06 20:21:02 +0200

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send header in curl GET request
-curl -s --data "email=x@mariam.com&subject=I will always be here for PLD" "$1"
+# takes in a URL, sends a POST
+curl "$1" -sX POST -d "email=hr@mariam.com&subject=I will always be here for PLD"
